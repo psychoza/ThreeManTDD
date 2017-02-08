edcde3e7676b430b7f35dc65acc55fdee234b8e5
Jesse Hutchison <j.w.hutchison@live.com>
2017-02-08 17:08:55 -0500

    Commited the blank project :facepunch:
    Do it :fire:

---
+++ scripts/threeMan.js
@@ -0,0 +1,6 @@
+(function(tmg) {
+    tmg.game = function() {
+        var self = this;
+        return self;
+    };
+})(window.threeManGame = window.threeManGame || {});
---
+++ scripts/threeMan.tests.js
@@ -0,0 +1,11 @@
+window.threeManGame = window.threeManGame || {};
+
+describe('The Three Man Game - ', function (){
+  var game = new window.threeManGame.game();
+
+	it('must exist - ', function(){
+    expect(game !== undefined).toBe(true);
+	});
+  
+  // it ('', function() {});
+});
